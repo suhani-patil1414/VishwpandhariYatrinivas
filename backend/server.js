2d2aadcd6257ebc340baeb736481cd15be66626f
suhani-patil1414 <suhanipatil0525@gmail.com>
2026-06-03 15:34:09 +0530

    Render fix

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -24,6 +25,7 @@ const app = express();
 
 app.use(cors());
 // app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend")));
 app.use(express.json({
   limit: "50mb"
 }));
@@ -990,11 +992,17 @@ app.delete("/delete-enquiry/:id", (req, res) => {
 
 // ================= HOME =================
 app.get("/", (req, res) => {
-  res.send("Vishwpandhari Yatrinivas Server Running ✅");
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
 
 // ================= SERVER =================
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000 🚀");
+// app.listen(5000, () => {
+//   console.log("Server running on http://localhost:5000 🚀");
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
